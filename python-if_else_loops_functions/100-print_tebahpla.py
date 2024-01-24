50f5a824d72bdea198cf2a177975134735453ab4
emma-33 <emma.lateyron@orange.fr>
2024-01-24 14:24:51 +0100

    fix 100-print_tebahpla.py : format in print

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for num in range(122, 96, -1):
-    print(chr(num) if num % 2 == 0 else chr(num-32), end="")
+for char in range(122, 96, -1):
+    print("{:c}".format(char if (char % 2 == 0) else (char - 32)), end="")
