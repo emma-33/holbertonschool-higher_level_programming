a13c8a14c9b7f52abc4c05f69f8bbecb7d7fd325
emma-33 <emma.lateyron@orange.fr>
2024-01-25 11:14:31 +0100

    fix : 100-my_calculator.py from sys import argv

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, div, mul
 
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1
+    num_args = len(argv) - 1
 
     if num_args != 3:
-        print(num_args)
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        operator = sys.argv[2]
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print(num_args)
+        operator = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
 
         if operator == "+":
             print("{:d} {} {:d} = {:d}".format(a, operator, b, add(a, b)))
