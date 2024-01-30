6a70c71a7eaed2a2b62de016f187e50fc83de926
emma-33 <emma.lateyron@orange.fr>
2024-01-31 00:21:14 +0100

    fix : 0-square_matrix_simple.py - remove .append

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
+    new_matrix = matrix.copy()
 
-    if len(matrix) > 0:
-        for i in matrix:
-            new_matrix.append(list(map(lambda x: x ** 2, i)))
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
 
     return new_matrix
