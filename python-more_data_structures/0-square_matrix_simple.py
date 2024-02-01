5590f07d45897a882c4d6fc5ab5f679c5d9500ba
emma-33 <emma.lateyron@orange.fr>
2024-02-01 22:01:06 +0100

    fix : 0-square_matrix_simple.py ; add if not matrix stat

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,10 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
 
-    if len(matrix) > 0:
-        for i in matrix[:]:
-            new_matrix.append(list(map(lambda x: x ** 2, i)))
+    if not matrix:
+        return new_matrix
+
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: x ** 2, i)))
 
     return new_matrix
