13a4a625ba3e9c9a06d8e9f8c1b702ce77b03e1d
emma-33 <emma.lateyron@orange.fr>
2024-02-01 23:02:11 +0100

    fix : 7-add_tuple.py ; remove tuple map lambda + change order if else

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    result = 0
     if len(tuple_a) < 2:
-        if len(tuple_a) == 1:
-            tuple_a = tuple_a[0], 0
-        else:
+        if len(tuple_a) == 0:
             tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
 
     if len(tuple_b) < 2:
-        if len(tuple_b) == 1:
-            tuple_b = tuple_b[0], 0
-        else:
+        if len(tuple_b) == 0:
             tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
 
-    result = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
-    return(result)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
