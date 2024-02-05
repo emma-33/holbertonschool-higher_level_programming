76f1f713662ffc8383e3c9b28da489882fc6b647
emma-33 <emma.lateyron@orange.fr>
2024-02-05 15:09:04 +0100

    add : 1-main.py 1-square.py update : README.md

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square
+    """
+    def __init__(self, size):
+        """Initializing size
+        Args :
+            size: size of square
+        """
+        self.__size = size
--- python-classes/README.md
+++ python-classes/README.md
@@ -1,1 +1,3 @@
 Task 0 : defines an empty class Square that defines a square  
+Task 1 : defines a square with size  
+ 
