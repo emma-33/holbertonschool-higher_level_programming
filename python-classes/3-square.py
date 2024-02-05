633cafd9e64216b9a70900603baf8c0d08dae903
emma-33 <emma.lateyron@orange.fr>
2024-02-05 15:47:50 +0100

    add : 3-main.py 3-square.py update : README.md fix : 2-square.py ; move self.__size = size at the end

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """defines a square
-    """
+    """defines a square"""
     def __init__(self, size=0):
         """Initializing size
         Args :
@@ -11,6 +10,9 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
-        
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
--- python-classes/README.md
+++ python-classes/README.md
@@ -1,3 +1,4 @@
 Task 0 : defines an empty class Square that defines a square  
 Task 1 : defines a square with size  
 Task 2 : defines a square with raised exceptions and messages  
+Task 3 : defines a square with public instance method area that returns the current square area  
