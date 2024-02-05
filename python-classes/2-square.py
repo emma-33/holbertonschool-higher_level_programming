66637d055dc458499e8338351d72223fadc16a4f
emma-33 <emma.lateyron@orange.fr>
2024-02-05 15:34:19 +0100

    add 2-main.py 2-square.py update : README.md

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square
+    """
+    def __init__(self, size=0):
+        """Initializing size
+        Args :
+            size: size of square
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
--- python-classes/README.md
+++ python-classes/README.md
@@ -1,3 +1,3 @@
 Task 0 : defines an empty class Square that defines a square  
 Task 1 : defines a square with size  
- 
+Task 2 : defines a square with raised exceptions and messages  
