d0ea168390507d608216f778a3a1098704b5724b
emma-33 <emma.lateyron@orange.fr>
2024-02-06 18:32:06 +0100

    add : 4-main.py 4-print_square.py 4-print_square.txt update : README.md

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+
+Defines a function that prints a square with #.
+
+"""
+
+
+def print_square(size):
+    """ Function that prints a square with #.
+    Args:
+        size: size of square
+
+    Raises:
+        TypeError: If size isn't an integer
+        ValueError: If size is less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
--- python-test_driven_development/README.md
+++ python-test_driven_development/README.md
@@ -1,3 +1,4 @@
 Task 0 : adds two integers  
 Task 1 : divides all elements in a matrix  
-Task 2 : prints My name is <first name> <last name>  
+Task 2 : prints My name is 'first name' 'last name'  
+Task 3 : prints a square with #  
