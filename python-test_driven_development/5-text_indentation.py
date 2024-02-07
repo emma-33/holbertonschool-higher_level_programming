d61013e22cddccddff59d2eec29d6373212624e2
emma-33 <emma.lateyron@orange.fr>
2024-02-07 14:27:03 +0100

    add : 5-main.py 5-text_indentation.py 5-text_indentation.txt update : README.md

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+
+Defines a function that prints a text with 2 newlines after each of
+these characters . ? and : .
+
+"""
+
+
+def text_indentation(text):
+    """ Function that prints a text with 2 newlines after each of
+    these characters . ? and :.
+    Args:
+        text: text to print
+
+    Raises:
+        TypeError: If text isn't a string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be an string")
+
+    i = 0
+    while i < len(text):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(text[i])
+            print("")
+            i += 1
+        else:
+            print(text[i], end='')
+        i += 1
--- python-test_driven_development/README.md
+++ python-test_driven_development/README.md
@@ -2,3 +2,4 @@ Task 0 : adds two integers
 Task 1 : divides all elements in a matrix  
 Task 2 : prints My name is 'first name' 'last name'  
 Task 3 : prints a square with #  
+Task 4 : prints a text with 2 new lines after each of these characters: ., ? and :  
