ce70dded478802ed5e8b850b8d24d6167371266d
emma-33 <emma.lateyron@orange.fr>
2024-02-08 17:47:27 +0100

    fix : 6-square.py ; name position.setter instead of size.setter and add if in position.setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,9 +31,15 @@ class Square:
         """Retrieves the position."""
         return self.__position
 
-    @size.setter
+    @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
