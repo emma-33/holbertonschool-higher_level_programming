dc35090dd211ad8233fcc10374a157ceb5983d04
emma-33 <emma.lateyron@orange.fr>
2024-02-09 14:45:12 +0100

    fix 4-print_square.py ; modify raise Value Error

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -20,7 +20,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
 
     if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
+        raise ValueError("size must be >= 0")
 
     if size < 0:
         raise ValueError("size must be >= 0")
