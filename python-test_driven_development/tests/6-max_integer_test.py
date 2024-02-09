55530b867795538860d6c93d19fe7325ac385cc1
emma-33 <emma.lateyron@orange.fr>
2024-02-09 14:04:18 +0100

    fix : 6-max_integer.py ; add test_

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,13 +7,23 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """test for max integer"""
 
-    def valid_input(self):
-        """tests valid inputs"""
+    def test_max_at_the_end(self):
+        """tests max at the end"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
     
-    def not_an_int(self):
+    def test_max_at_the_beginning(self):
+        """tests max at the beginning"""
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+        
+    def test_max_in_the_middle(self):
+        """tests max in the middle"""
+        self.assertEqual(max_integer([1, 4, 3, 2]), 4)
+    
+    def test_not_an_int(self):
         """tests for not an int"""
-        self.assertIsInstance(max_integer("Hello"), int)
+        with self.assertRaises(TypeError):
+            max_integer("Hello")
+
     
 if __name__ == '__main__':
     unittest.main()
