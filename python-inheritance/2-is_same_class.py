5a08ceb20261e4d85410861db0565b75e0bfe7db
emma-33 <emma.lateyron@orange.fr>
2024-02-14 11:10:04 +0100

    add 2-is_same_class.py update README.md

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a function that check if an object is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if obj is an instance of a_class
+
+    Args:
+        obj: object to check
+        a_class: class to compare to
+
+    Returns:
+        True if obj is an instance of a_class, otherwise False
+    """
+    if type(obj) == a_class:
+        return True
+    return False
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -1,1 +1,3 @@
 Function 0 : returns the list of available attributes and methods of an object  
+Function 1 : Defines a list "MyList" that inherits from list  
+Function 2 : Returns True if the object is exactly an instance of the specified class ; otherwise False  
