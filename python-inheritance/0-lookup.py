14eb7a51434cdd5c930ad3d83e1150f36a97ba5e
emma-33 <emma.lateyron@orange.fr>
2024-02-16 00:53:18 +0100

    add 12-pascal_triangle fix : 0-lookup.py ; doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Defines a function that returns
+the list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
     return dir(obj)
---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    new_list = []
+    if n <= 0:
+        return new_list
+    
