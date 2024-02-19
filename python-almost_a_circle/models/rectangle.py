2d226df6880e7b503d3e53df3d090433cf0c9fd7
emma-33 <emma.lateyron@orange.fr>
2024-02-19 12:31:33 +0100

    add rectangle.py and test_rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Define a rectangle Class"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represents the rectangle class"""
+    __nb_objects = 0
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializing the rectangle"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Returns current width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns current height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Unittest for rectangle class"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+class Test_Rectangle_instantiation(unittest.TestCase):
+    """test for instantiation of recatngle class"""
+    
+    def test_new_rectangle(self):
+        new = Rectangle(1, 3, 5, 4, 7)
+        self.assertEqual(new.width, 1)
+        self.assertEqual(new.height, 3)
+        self.assertEqual(new.x, 5)
+        self.assertEqual(new.y, 4)
+        self.assertEqual(new.id, 7)
+
+    
+    
+if __name__ == '__main__':
+    unittest.main()
