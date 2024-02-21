1a73c068fdc6ea942752bfb14f5d7c7bda7d895c
emma-33 <emma.lateyron@orange.fr>
2024-02-21 21:25:16 +0100

    add : __init__.py in tests fix : square.py;newline

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Represents the square class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize instances"""
         super().__init__(size, size, x, y, id)
