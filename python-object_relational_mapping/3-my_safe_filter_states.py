3e2225da423f02f9cc39237eb9070f786c96c969
emma-33 <emma.lateyron@orange.fr>
2024-03-22 13:10:07 +0100

    [add] 3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+
+import sys
+import MySQLdb
+
+def main():
+    """Main method to list all states where name ùatches the argument"""
+    if len(sys.argv) == 5:
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        
+        cur = db.cursor()
+        cur.execute("SELECT id, name FROM `states` WHERE name = '{}' ORDER BY id;".format(sys.argv[4]))
+
+        states = cur.fetchall()
+        for state in states:
+            print(state)
+        
+        cur.close()
+        db.close()
+    
+    else:
+        return
+
+if __name__ == '__main__':
+    main()
