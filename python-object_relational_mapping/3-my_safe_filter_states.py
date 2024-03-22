67c2a4b712400bac4f8a2ba5ac8c7e639cc8d063
emma-33 <emma.lateyron@orange.fr>
2024-03-22 23:32:42 +0100

    [fix] 3-my_safe_filter.py : use name = %s

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,24 +8,20 @@ import MySQLdb
 
 def main():
     """Main method to list all states where name matches the argument"""
-    if len(sys.argv) == 5:
-        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
 
-        cur = db.cursor()
-        cur.execute("""SELECT id, name FROM `states`
-                    WHERE name LIKE BINARY '{}'
-                    ORDER BY id;""".format(sys.argv[4]))
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM `states`
+                WHERE name = %s
+                ORDER BY id;""", (sys.argv[4],))
 
-        states = cur.fetchall()
-        for state in states:
-            print(state)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
 
-        cur.close()
-        db.close()
-
-    else:
-        return
+    cur.close()
+    db.close()
 
 
 if __name__ == '__main__':
