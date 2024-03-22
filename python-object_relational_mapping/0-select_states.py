3b2d1b4c2c7906d57d87bbc23db943ace32075fc
emma-33 <emma.lateyron@orange.fr>
2024-03-22 13:15:31 +0100

    [fix] 0-select_states.py 1-filter_states.py 2-my_filter_states.py 3-my_safe_filter_states.py : pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,19 +4,22 @@
 import sys
 import MySQLdb
 
+
 def main():
     """Main method to list all states"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
     cur.execute("SELECT id, name FROM `states` ORDER BY id;")
 
     states = cur.fetchall()
     for state in states:
         print(state)
-    
+
     cur.close()
     db.close()
-    
+
+
 if __name__ == '__main__':
     main()
